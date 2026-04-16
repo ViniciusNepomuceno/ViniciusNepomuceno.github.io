f11ad136dca95d8f5c94117ef9ed2e895a199ad8
ViniciusNepomuceno <viniciusnepomuceno35@gmail.com>
2026-04-16 15:48:07 -0300

    Segundo commit

--- programacao_para_web/dia 11 de fevereiro/script.js
+++ programacao_para_web/dia 11 de fevereiro/script.js
@@ -1,7 +1,7 @@
 let nome = "Vinicius";
 let sobrenome = "Nepomuceno"
 console.log("Meu nome é " + nome + " " + sobrenome);
-console.log("Sou de Horizonte, Yasmin case comigo S2")
+console.log("Sou de Horizonte")
 
 const PI = 3.14;
 console.log(PI)
